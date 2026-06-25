7d6a54bd4ddfa9f62879f4c1568c79ff8ecc7382
Gouransh <gouranshgupta01@gmail.com>
2026-06-25 12:32:27 +0530

    feat(frontend): complete Feature and Solution Section of Landing Page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import HeroSection from '../components/landing/HeroSection';
 import ProblemSection from '../components/landing/ProblemSection';
-
+import SolutionSection from '../components/landing/SolutionSection';
+import FeatureSection from '../components/landing/FeatureSection';
 const Landing = () => {
   return (
     <div className="min-h-screen bg-primary-950 text-white selection:bg-brand-500/30">
@@ -9,6 +10,8 @@ const Landing = () => {
       <HeroSection />
 
       <ProblemSection />
+      <SolutionSection />
+      <FeatureSection />
     </div>
   );
 };
